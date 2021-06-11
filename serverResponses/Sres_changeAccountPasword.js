6e42b00fde58c75a619ba5e8297867a576e99fef
KinDom2 <83883077+KinDom2@users.noreply.github.com>
2021-06-11 21:17:25 +0200

    Update Sres_changeAccountPasword.js

--- serverResponses/Sres_changeAccountPasword.js
+++ serverResponses/Sres_changeAccountPasword.js
@@ -5,6 +5,15 @@ let SHA256 = require("crypto-js/sha256");
 //pool object - póla połączeń z bazą mysql, z której wydzielane jest połączenie względem zapotrzebowania i możliwości serwera
 //res function - funkcja odsyłająca pakiety danych do klienta
 //params object - zbiór parametrów w postaci obiektu
+/**
+ *
+ * Funckja Sres_changeAccountPasword która pobiera specjalnie stworzony Sres_Promise i odsyła jego wynik.<br>
+ * 
+ * @param {object} pool - póla połączeń z bazą mysql, z której wydzielane jest połączenie względem zapotrzebowania i możliwości serwera.
+ * @param {res} function -funkcja odsyłająca pakiety danych do klienta.
+ * @param {object} params - zbiór parametrów w postaci obiektu.
+ *
+ */
 function Sres_changeAccountPasword(pool, res, params) {
 	//pobranie funkcji ServerResponse która pozawala na szybkie odesłanie danych
 	const { ServerResponse } = require('./ServerResponse');
@@ -14,7 +23,22 @@ function Sres_changeAccountPasword(pool, res, params) {
 	ServerResponse(contentCreator, res);
 }
 
+
+
 //pobranie póli połączeń oraz rozbicie (dekonstrukcja) parametrów przekazanych przez funkcję Sres
+
+/**
+ *
+ * Funckja Sres_promise która pobiera pulę połączeń oraz rozbija (dekonstrukcja) parametry przekazane przez funkcję Sres_changeAccountPasword.<br>
+
+ * 
+ * @param {object} pool - póla połączeń z bazą mysql, z której wydzielane jest połączenie względem zapotrzebowania i możliwości serwera.
+ * @param {object} { userId, password } - zbiór parametrów w postaci obiektu.
+ *
+ * @returns {Promise} - zwraca informację o zakończeniu operacji asynchronicznej.
+ *
+ */
+
 function Sres_promise(pool, { userId, password }) {
 
 	return new Promise((resolve, reject) => {
